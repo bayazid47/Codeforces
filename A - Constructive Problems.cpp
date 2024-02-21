9a489622fa7b636738726a9eb9fd171a70709e85
Bayazid Hosen <102893528+bayazid47@users.noreply.github.com>
2024-02-21 14:00:49 +0600

    A - Constructive Problems.cpp

---
+++ A - Constructive Problems.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+#define ll long long
+
+
+using namespace std;
+
+int32_t main () {
+    int t;
+    cin>>t;
+
+    while(t--){
+       int n,m;
+       cin>>n>>m;
+       cout<<max(n,m)<<endl;
+       
+    }
+    return 0;
+}
